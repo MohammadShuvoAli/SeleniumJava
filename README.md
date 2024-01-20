75e0b24d04e9f787c69b736663fe56a1b4047315
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-01-21 00:13:07 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SeleniumJava
+ Selenium Java
---
+++ SeleniumJava/src/com/selenium/java/OpenGoogle.java
@@ -0,0 +1,27 @@
+package com.selenium.java;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenGoogle {
+
+    public static void main(String[] args) {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
+
+        // Create a new instance of the ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to Google
+        driver.get("https://www.google.com");
+
+        // Compare the title using equals()
+        if (driver.getTitle().equals("Google"))
+            System.out.println("Test Passed!!!");
+        else
+            System.out.println("Test Failed!!!");
+
+        // Close the browser
+        driver.quit();
+    }
+}
